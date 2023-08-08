181950ec3dda4d916881959b0c1e192acb097c54
Harsh Agrawal <122919453+Harsh9174@users.noreply.github.com>
2023-08-08 23:56:36 +0530

    Update Hr_name.py

--- web scraping/Hr_name.py
+++ web scraping/Hr_name.py
@@ -1,51 +1,51 @@
 import pandas as pd
-import numpy as np
-import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+import time
 
-
+# Load data from a CSV file and clean it by dropping rows with missing values
 df = pd.read_csv("A:/Masai Projects/ML Project Job ANAlytics/Job_Data.csv")
 df.dropna(inplace=True)
-df.to_csv("A:/Masai Projects/ML Project Job ANAlytics/Job_Datacleaned.csv",index=False)
+df.to_csv("A:/Masai Projects/ML Project Job ANAlytics/Job_Datacleaned.csv", index=False)
 
-
+# Extract the 'Link' column values from the DataFrame
 Link_L = []
 for i in df['Link']:
     Link_L.append(i)
-service = Service('C:/Users/Mi/Downloads/chromedriver_win32/chromedriver.exe')
+
+# Set up Selenium WebDriver using Chrome
 driver = webdriver.Chrome(executable_path=r'C:/Users/Mi/Downloads/chromedriver_win32/chromedriver.exe')
 
 HR_name = []
 Experience = []
 
-# link = 'https://www.instahyre.com/job-245061-data-scientist-engineering-at-google-2-bangalore-hyderabad/'
+# Loop through the list of links
 try:
     for link in Link_L:
         driver.get(link)
+        
+        # Find the HR name element on the page and append to HR_name list
         try:
-            hr_name = driver.find_element(By.CLASS_NAME,"rec-name").text
+            hr_name = driver.find_element(By.CLASS_NAME, "rec-name").text
             HR_name.append(hr_name)
             time.sleep(2)
         except:
             HR_name.append("NA")
         print(len(HR_name))
-
+        
+        # Find the Experience element on the page and append to Experience list
         try:
-            exp = driver.find_element(By.XPATH,'//*[@id="floating-header"]/div[1]/div/span[2]').text
+            exp = driver.find_element(By.XPATH, '//*[@id="floating-header"]/div[1]/div/span[2]').text
             Experience.append(exp)
         except:
             Experience.append("NA")
-        
 
 except:
     print("NA")
 
-df = pd.DataFrame({"HR_Name":HR_name,"Experience":Experience})
+# Create a new DataFrame with HR names and experience data
+df_hr_exp = pd.DataFrame({"HR_Name": HR_name, "Experience": Experience})
 
-df.to_csv("A:/Masai Projects/Job Analytics project/hr_exp.csv",index = False)
-print(df)
+# Save the new DataFrame to a CSV file
+df_hr_exp.to_csv("A:/Masai Projects/Job Analytics project/hr_exp.csv", index=False)
+print(df_hr_exp)
