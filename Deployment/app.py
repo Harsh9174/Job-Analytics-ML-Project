ccdd9b691e4a392bb365c1f3f3d6fb92f64147e1
Harsh Agrawal <122919453+Harsh9174@users.noreply.github.com>
2023-08-09 00:01:04 +0530

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 from spellchecker import SpellChecker
 
+# Load the cleaned dataset
 df2 = pd.read_csv("C:/Users/Mi/Downloads/Job Analytics/finalcleandata.csv")
+
+# Fill missing skill columns with empty strings and create lowercase versions for searching
 df2['Skills1'] = df2['Skills1'].fillna("")
 df2["lower_skills1"] = df2['Skills1'].str.lower()
 df2["lower_skills2"] = df2['Skills2'].str.lower()
@@ -12,36 +15,24 @@ df2["lower_skills5"] = df2['Skills5'].str.lower()
 df2["lower_skills6"] = df2['Skills6'].str.lower()
 df2["lower_skills7"] = df2['Skills7'].str.lower()
 
-
+# Streamlit configuration
 st.set_page_config(
     page_title="Job Analytics",
     page_icon="💼",
-    #initial_sidebar_state="collapsed",
 )
 
-
-    
-# st.markdown(""" <style>
-# #MainMenu {visibility: hidden;}
-# </style> """, unsafe_allow_html=True)
-
-# front end elements of the web page 
+# Frontend header
 html_temp = """ 
 <div style ="background-color:#3275a8;padding:8px"> 
 <h1 style ="font-family:Times New Roman;color:white;text-align:center;">Job Analytics</h1> 
 </div> 
 """
+st.markdown(html_temp, unsafe_allow_html=True)
 
+# Description of the search feature
+st.markdown("<h4> Our search feature displays the most prevalent experience level, top workplace, and popular classes for a particular skill, along with the total number of jobs related to that skill.</h4>", unsafe_allow_html=True)
 
-# display the front end aspect
-st.markdown(html_temp, unsafe_allow_html = True)
-
-#st.write("*Our search feature displays the most prevalent experience level, top workplace, and popular classes for a particular skill, along with the total number of jobs related to that skill.*")
-
-st.markdown("<h4> Our search feature displays the most prevalent experience level, top workplace, and popular classes for a particular skill, along with the total number of jobs related to that skill.</h>", unsafe_allow_html=True)
-
-
-
+# Load skills from a dictionary
 with open('C:/Users/Mi/Downloads/Job Analytics/dictionary.txt') as f:
     skills = f.read().splitlines()
 
@@ -49,12 +40,14 @@ with open('C:/Users/Mi/Downloads/Job Analytics/dictionary.txt') as f:
 spell = SpellChecker(language=None, case_sensitive=False)
 spell.word_frequency.load_words(skills)
 
-
+# Text input for skill search
 text_search = st.text_input("**Search Skills**", value="")
 
 if text_search:
     corrected_input = spell.correction(text_search)
     st.write(f'Searching for jobs related to: {corrected_input}')
+    
+    # Create masks for skills in different columns
     m1 = df2["lower_skills1"].str.contains(corrected_input)
     m2 = df2["lower_skills2"].str.contains(corrected_input)
     m3 = df2["lower_skills3"].str.contains(corrected_input)
@@ -63,22 +56,18 @@ if text_search:
     m6 = df2["lower_skills6"].str.contains(corrected_input)
     m7 = df2["lower_skills7"].str.contains(corrected_input)
 
-
+    # Combine masks to get the search results
     df_search = df2[m1 | m2 | m3 | m4 | m5 | m6 | m7]
-    #.drop('Skills1',axis=1)
     df_search = df_search.reset_index(drop=True)
     df_search.index += 1
 
-
-
     if corrected_input:
-        col1,col2 = st.columns(2)
+        col1, col2 = st.columns(2)
         with col1:
             st.markdown('<h4 style="color: black">Most Common Experience Level :</h4>', unsafe_allow_html=True)
             st.markdown('<h4 style="color: black">Most Common Workplace :</h4>', unsafe_allow_html=True)
             st.markdown('<h4 style="color: black">Most Common Classes :</h4>', unsafe_allow_html=True)
             st.markdown('<h4 style="color: black">Total Number of Jobs Available :</h4>', unsafe_allow_html=True)
-               
         with col2:
             exp = df_search['Experience Category'].mode()[0]
             st.markdown(f'<h4 style="color: green">{exp}</h4>', unsafe_allow_html=True)
@@ -92,71 +81,49 @@ if text_search:
             num_jobs = str(len(df_search))
             st.markdown(f'<h4 style="color: green">{num_jobs}</h4>', unsafe_allow_html=True)
 
+# Display job details
+st.markdown(
+    """
+    <style>
+        .stButton>button {
+            background-color: #3275a8;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
+if st.button("Job Details >>"):
+    data = pd.concat([df_search['Company'], df_search['Designation'], df_search['Locations'].rename("Workplace"),df_search['HR_Name'].rename("HR Name"),df_search['Experience'],df_search['Link'].rename("Apply Link")], axis=1)
+    st.write(data)
     
-    # if corrected_input:
-        # exp = df_search['Experience Category'].mode()[0]
-        # st.subheader(f'**Most Common Experience Level** : **:green[{exp}]**')
-        
-        # loc = df_search['Locations'].mode()[0]
-        # st.subheader(f'**Most Common Workplace** : **:green[{loc}]**')
-        
-        # cls = df_search['Cluster'].mode()[0]
-        # st.subheader(f'**Most Common Classes** : **:green[{cls}]**')
-    
-        # num_jobs = len(df_search)
-        # st.subheader(f'**Total Number of Jobs Available** : **:green[{num_jobs}]**')
-        
-        
-# col1, col2, col3, col4, col5 = st.columns(5)
+    # Card style for displaying job details
+    card_style = """
+    <style>
+    .card {
+        display: inline-block;
+        padding: 1rem;
+        margin: 1rem;
+        background-color: #f0f0f0;
+        border-radius: 0.5rem;
+        width: 650px;
+        height: 310px;
+    }
+    </style>
+    """
+    st.markdown(card_style, unsafe_allow_html=True)
 
-# with col5:
-        st.markdown(
-            """
-            <style>
-                .stButton>button {
-                    background-color: #3275a8;
-                }
-            </style>
-            """,
-            unsafe_allow_html=True,
-        )
-        if st.button("Job Details >>"):
-            #subprocess.Popen(["streamlit", "run", "job_info.py"])
-            
-            data = pd.concat([df_search['Company'], df_search['Designation'], df_search['Locations'].rename("Workplace"),df_search['HR_Name'].rename("HR Name"),df_search['Experience'],df_search['Link'].rename("Apply Link")], axis=1)
-            st.write(data)
-            
-            # Define card style
-            card_style = """
-            <style>
-            .card {
-                display: inline-block;
-                padding: 1rem;
-                margin: 1rem;
-                background-color: #f0f0f0;
-                border-radius: 0.5rem;
-                width: 650px;
-                height: 310px;
-            }
-            </style>
-            """
-
-            # Add card style to app
-            st.markdown(card_style, unsafe_allow_html=True)
-
-            # Loop through records and create cards
-            for index, row in data.iterrows():
-                card = """
-                <div class="card">
-                    <h4>Company Name  :  {}</h4>
-                    <h5>Designation    :  {}</h5>
-                    <h5>Workplace      :  {}</h5>   
-                    <h5>HR Name        :  {}</h5>
-                    <h5>Experience     :  {}</h5>
-                    <a href="{link}">Click Here to Apply</a>
-                </div>
-                """.format(row['Company'], row['Designation'], row['Workplace'], row['HR Name'], row['Experience'], link=row['Apply Link'])
-                
-                st.markdown(card, unsafe_allow_html=True)
-
+    # Loop through records and create job cards
+    for index, row in data.iterrows():
+        card = """
+        <div class="card">
+            <h4>Company Name  :  {}</h4>
+            <h5>Designation    :  {}</h5>
+            <h5>Workplace      :  {}</h5>   
+            <h5>HR Name        :  {}</h5>
+            <h5>Experience     :  {}</h5>
+            <a href="{link}">Click Here to Apply</a>
+        </div>
+        """.format(row['Company'], row['Designation'], row['Workplace'], row['HR Name'], row['Experience'], link=row['Apply Link'])
+        
+        st.markdown(card, unsafe_allow_html=True)
